f903dae096e80af93a79e9a467dd31102170c9fc
Kubatbekkk <kubineccastro@gmail.com>
2023-09-15 21:06:36 +0600

    chore: change port to be default 5173

---
+++ src/features/books/index.ts
@@ -0,0 +1,8 @@
+import BookCard from './components/BookCard';
+import BookDetails from './components/BookDetails';
+import Books from './components/Books';
+import BooksList from './components/BooksList';
+import Header from './components/Header';
+import Home from './components/Home';
+
+export { BookCard, BookDetails, Books, BooksList, Header, Home };
